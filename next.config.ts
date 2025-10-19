a06895d8a2fc96598a772142ddf43070ede1147d
Viktor Rozaev <vrozaev@tracto.ai>
2025-10-19 14:58:59 +0200

    Fixed path to site images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: {unoptimized: true},
+  basePath: '/aleksandra-website'
 };
 
 export default nextConfig;
