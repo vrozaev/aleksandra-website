3cb47b31ab8cd335094821fac5a5396d36af7101
Viktor Rozaev <vrozaev@tracto.ai>
2025-10-26 09:37:55 +0100

    Services page draft

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,10 +1,32 @@
+import { Text } from "@/ui/components/Text/Text";
 import styles from "./page.module.css";
+import { Button } from "@/ui/components/Button/Button";
+import { BlockTextAndImage } from "@/ui/blocks/BlockTextAndImage/BlockTextAndImage";
+import { Link } from "@/ui/components/Link/Link";
+
+const CTAButton: React.FC<{children: React.ReactNode;}> = ({children}) => (
+  <Button className={styles.page__cta}>{children}</Button>
+);
+
+const TEXT_AND_IMAGES_PROPS = {
+  image: '/aleksandra-website/pages/about/about.webp',
+  title: 'Aleksandra Borisova',
+  paragraphs: [
+    `TODO: paragraph 1.`,
+    `TODO: paragraph 2.`,
+    `TODO: paragraph 3.`,
+  ],
+}
 
 export default function Services() {
   return (
     <div className={styles.page}>
+      <Text variant='h2' className={styles.page__title}>OUR SERVICES</Text>
       <main>
-        TODO: services page
+        <BlockTextAndImage 
+          {...TEXT_AND_IMAGES_PROPS}
+        />
+        <Link href='/contact'><CTAButton>Get in touch</CTAButton></Link>
       </main>
     </div>
   );
