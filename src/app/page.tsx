924c6a4b445625aa2c77762f4581f1a0a1ae1bb8
Rozaev Viktor <vrozaev@tracto.ai>
2025-10-29 17:53:25 +0100

    Introduced basic index page (#11)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
+import { Text } from "@/ui/components/Text/Text";
 import styles from "./page.module.css";
+import { BlockProjects } from "@/ui/blocks/BlockProjects/BlockProjects";
+import { PROJECTS_LIST } from "@/shared/projects";
+import { Spacer } from "@/ui/components/Spacer/Spacer";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main>
-        TODO: home page
+        <div className={styles.page__mainBlock}>
+          <div className={styles.page__mainBlockTextWrapper}>
+            <Text variant="h1">Aleksandra Borisova Studio</Text>
+            <Text variant="h2">Interior design, renovation, supervision</Text>
+          </div>
+        </div>
+        <Spacer size='m' />
+        <Text variant='h2' className={styles.page__lastetsProjectsTitle}>Latest Projects</Text>
+        <Spacer size='m' />
+        <BlockProjects projects={PROJECTS_LIST} />
       </main>
     </div>
   );
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import { Button } from "@/ui/components/Button/Button";
 import { BlockTextAndImage } from "@/ui/blocks/BlockTextAndImage/BlockTextAndImage";
 import { Link } from "@/ui/components/Link/Link";
+import { Spacer } from "@/ui/components/Spacer/Spacer";
 
 const CTAButton: React.FC<{children: React.ReactNode;}> = ({children}) => (
   <Button className={styles.page__cta}>{children}</Button>
@@ -21,7 +22,9 @@ const TEXT_AND_IMAGES_PROPS = {
 export default function Services() {
   return (
     <div className={styles.page}>
+      <Spacer size="m" />
       <Text variant='h2' className={styles.page__title}>OUR SERVICES</Text>
+      <Spacer size="m" />
       <main>
         <BlockTextAndImage 
           {...TEXT_AND_IMAGES_PROPS}
