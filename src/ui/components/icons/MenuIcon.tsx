d40b88c635b85d87b98a901825198f0e33db47a5
Rozaev Viktor <vrozaev@tracto.ai>
2025-10-29 17:24:14 +0100

    Mobile navigation & design review (#10)

---
+++ src/ui/components/icons/MenuIcon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from './Icon.module.css';
+import { cc } from "../utils/cc";
+
+
+type MenuIconProps = {
+    onClick?: () => void;
+    className?: string;
+};
+
+export const MenuIcon: React.FC<MenuIconProps> = ({onClick, className}) => {
+    return (
+        <svg 
+            className={cc(styles.icon, className)}
+            onClick={onClick}
+            fill="#000000" 
+            width="30px" 
+            height="30px" 
+            viewBox="0 0 20 20" 
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+        </svg>
+    );
+}
