47864381b96c9944099ca6f6ed5529ff4f62744f
Viktor Rozaev <vrozaev@tracto.ai>
2025-11-01 11:21:25 +0100

    draft

--- src/ui/blocks/ProjectBlock/ProjectBlock.tsx
+++ src/ui/blocks/ProjectBlock/ProjectBlock.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import styles from './ProjectBlock.module.css';
+import { Text } from "@/ui/components/Text/Text";
+import Image, { StaticImageData } from "next/image";
 
 type ProjectBlockProps = {
-    images: string[];
+    images: StaticImageData[];
     name: string;
     details: string;
     description: string[];
-}
+};
 
-export const ProjectBlock: React.FC<ProjectBlockProps> = () => {
+export const ProjectBlock: React.FC<ProjectBlockProps> = ({images, name, details, description}) => {
     return (
         <div className={styles.projectBlock}>
-            
+            <div>
+                    {
+                        images.map((item, index) => (
+                            <Image src={item} key={index} alt='' />
+                        ))
+                    }
+            </div>
+            <div>
+                <Text variant="body" className={styles.projectBlock__name}>{name}</Text>
+                <Text variant="body" className={styles.projectBlock__details}>{details}</Text>
+                {
+                    description.map((item, index) => (
+                        <Text variant="body" key={index} className={styles.projectBlock__description}>{item}</Text>
+                    ))
+                }
+            </div>
         </div>
     )
 };
