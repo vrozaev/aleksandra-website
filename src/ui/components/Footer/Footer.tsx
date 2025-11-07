bd09d9dbebba6a8cd027a055e96b777feaeba8eb
Viktor Rozaev <vrozaev@tracto.ai>
2025-11-07 19:46:15 +0100

    Contact page fixes

--- src/ui/components/Footer/Footer.tsx
+++ src/ui/components/Footer/Footer.tsx
@@ -9,8 +9,8 @@ export const Footer: React.FC = () => {
             <div className={styles.footer__columnsWrapper}>
                 <div className={styles.footer__column}>
                     <div className={styles.footer__columnTitle}>Aleksandra Borisova Studio</div>
-                    <div>Email: <Link href='mailto:alexandra090593@gmail.com' view='footer'>alexandra090593@gmail.com</Link></div>
-                    <div>Phone: <Link href='https://wa.me/31645354483' view='footer'>+31 6 45 35 44 83</Link></div>
+                    <Link href='mailto:alexandra090593@gmail.com' view='footer'>alexandra090593@gmail.com</Link>
+                    <Link href='https://wa.me/31645354483' view='footer'>+31 6 45 35 44 83</Link>
                 </div>
                 <div className={styles.footer__column}>
                     <div className={styles.footer__columnTitle}>Follow us</div>
