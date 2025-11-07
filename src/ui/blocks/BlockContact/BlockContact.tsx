3816efc52d1911a7b37aa0b95a455b52762663ce
Rozaev Viktor <vrozaev@tracto.ai>
2025-11-07 19:46:35 +0100

    Contact page fixes (#17)

--- src/ui/blocks/BlockContact/BlockContact.tsx
+++ src/ui/blocks/BlockContact/BlockContact.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 import styles from './BlockContact.module.css';
 import { Link } from "@/ui/components/Link/Link";
+import { Text } from "@/ui/components/Text/Text";
 
 export const BlockContact: React.FC = () => {
     return (
         <div className={styles.blockContact}>
             <div className={styles.blockContact__row}>
-                <div className={styles.blockContact__rowLabel}>Email:</div>
-                <Link className={styles.blockContact__rowValue} href='mailto:alexandra090593@gmail.com' view='footer'>alexandra090593@gmail.com</Link>
+                <Text variant="body">Email</Text> <Link href='mailto:alexandra090593@gmail.com' view='footer'>alexandra090593@gmail.com</Link>
             </div>
             <div className={styles.blockContact__row}>
-                <div className={styles.blockContact__rowLabel}>Instagram:</div>
-                <Link className={styles.blockContact__rowValue} href='https://www.instagram.com/sanchita_arch' view='footer'>sanchita_arch</Link>
-            </div>
-            <div className={styles.blockContact__row}>
-                <div className={styles.blockContact__rowLabel}>Whataspp:</div>
-                <Link className={styles.blockContact__rowValue} href='https://wa.me/31645354483' view='footer'>+31 6 45 35 44 83</Link>
-            </div>
-            <div className={styles.blockContact__row}>
-                <div className={styles.blockContact__rowLabel}>Phone:</div>
-                <Link className={styles.blockContact__rowValue} href='tel:+4733378901' view='footer'>+31 6 45 35 44 83</Link>
+                <Text variant="body">Phone</Text> <Link href='https://wa.me/31645354483' view='footer'>+31 6 45 35 44 83</Link>
             </div>
         </div>
     );
