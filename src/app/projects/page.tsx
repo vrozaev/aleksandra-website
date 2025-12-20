3348c4e649c691cee0ba28c419b9889aa5773eee
Viktor Rozaev <vrozaev@tracto.ai>
2025-12-20 14:29:09 +0100

    rename blocks

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,11 @@
-import { BlockProjects } from "@/ui/blocks/BlockProjects/BlockProjects";
+import { BlockProjectsList } from "@/ui/blocks/BlockProjectsList/BlockProjectsList";
 import { PROJECTS_LIST } from "@/shared/projects";
 import { Page } from "@/ui/components/Page/Page";
 
 export default function Projects() {
   return (
     <Page title="Projects" maxWidth="l">
-        <BlockProjects projects={PROJECTS_LIST} />
+        <BlockProjectsList projects={PROJECTS_LIST} />
     </Page>
   );
 }
