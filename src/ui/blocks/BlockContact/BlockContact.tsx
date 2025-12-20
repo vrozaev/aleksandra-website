683633f40f713b0c393f5338c9d039cccc433474
Viktor Rozaev <vrozaev@tracto.ai>
2025-12-20 14:21:30 +0100

    Fixed contacts on mobile page

--- src/ui/blocks/BlockContact/BlockContact.tsx
+++ src/ui/blocks/BlockContact/BlockContact.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import styles from './BlockContact.module.css';
 import { Link } from "@/ui/components/Link/Link";
-import { Text } from "@/ui/components/Text/Text";
 
 export const BlockContact: React.FC = () => {
     return (
         <div className={styles['block-contact']}>
             <div className={styles['block-contact__row']}>
-                <Text variant="body">Email</Text> <Link href='mailto:alexandra090593@gmail.com' view='footer'>alexandra090593@gmail.com</Link>
+                <Link href='mailto:alexandra090593@gmail.com' view='footer'>alexandra090593@gmail.com</Link>
             </div>
             <div className={styles['block-contact__row']}>
-                <Text variant="body">Phone</Text> <Link href='https://wa.me/31645354483' view='footer'>+31 6 45 35 44 83</Link>
+                <Link href='https://wa.me/31645354483' view='footer'>+31 6 45 35 44 83</Link>
             </div>
         </div>
     );
