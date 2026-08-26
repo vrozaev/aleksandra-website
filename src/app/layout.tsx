368972a1bd1f431b1e11bb4bfc6ee1b52248f5d0
Viktor Rozaev <vrozaev@nebius.com>
2026-08-26 21:50:25 +0200

    Added fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,21 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Footer } from "@/ui/components/Footer/Footer";
 import { Navigation } from "@/ui/components/Navigation/Navigation";
-import { Raleway } from 'next/font/google'
+import { Alata, Source_Code_Pro } from "next/font/google";
 import { Content } from "@/ui/components/Content/Content";
- 
-const raleway = Raleway({
-  subsets: ['latin', 'cyrillic'],
-})
+
+const alata = Alata({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-alata",
+  display: "swap",
+});
+
+const sourceCodePro = Source_Code_Pro({
+  subsets: ["latin"],
+  variable: "--font-source-code-pro",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Aleksandra Borisova Studio",
@@ -20,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={raleway.className}>
+    <html lang="en" className={`${alata.variable} ${sourceCodePro.variable}`}>
       <body>
         <Navigation />
         <Content>
